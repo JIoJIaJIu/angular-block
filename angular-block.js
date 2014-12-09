49e77bc1af781976fb4d54cb2d108ce26a6aca63
Guro Bokum <jiojiajiu@gmail.com>
2014-12-09 19:33:32 +0300

    Skelet

--- angular-block.js
+++ angular-block.js
@@ -1,13 +1,39 @@
 (function (undefined) {
 
+angular.module.directive('block', ['$injector', '$templateCache', function ($injector, $templateCache) {
+    return {
+        restrict: 'A',
+        priority: 10,
+        replace: true,
+        transclude: true,
+        scope: {},
+
+        template: function (element, attrs) {},
+
+        compile: function () {
+            var self = this;
+            var directive;
+
+            return {
+                pre: function (scope, element, attrs, controller, transFn) {},
+
+                post: function (scope, element, attrs, controller, transFn) {}
+            }
+        }
+    }
+}])
+
 var angularModule = angular.module;
-
 angular.module = function angularDecorator () {
+    if (arguments.length === 1)
+        return angularModule.apply(angular, arguments);
 
-if (arguments.length === 1)
-    return angularModule.apply(angular, arguments);
+    var module = angularModule.apply(angular, arguments);
 
-var module = angularModule.apply(angular, arguments);
+    module.block = function module_block(name, constructor, mods) {
+    }
 
-return module;
+    return module;
+}
+
 })
